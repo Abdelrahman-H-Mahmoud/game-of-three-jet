2af9fde0664caa3e0a154965582586a42b685439
Abdelrahman Hussien <abdelrahman.h.mahmoued@gmail.com>
2024-02-17 23:11:45 +0200

    feat: make allowed numbers to play configurable

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 export const config = {
   port: process.env.PORT || 3000,
   notificationURL: process.env.NOTIFICATION_BASE_URL || "http://localhost:3000/subscribe",
-  numberOfPlayersPerGame: parseInt(process.env.NUMBER_OF_PLAYER_PER_GAME ?? "2")
+  numberOfPlayersPerGame: parseInt(process.env.NUMBER_OF_PLAYER_PER_GAME ?? "2"),
+  allowedNumber: [1, 0, -1]
 }
